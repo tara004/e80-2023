76f3b291deeec69256c6bbf2b9d2d34f620f1601
tara004 <singhtarnjot99@gmail.com>
2023-11-23 17:47:55 +0100

    per prog(benn)

---
+++ per progetto/HomeView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectWork_E80_2023.Views
+{
+    /// <summary>
+    /// Logica di interazione per HomeView.xaml
+    /// </summary>
+    public partial class HomeView : Page
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void Clients_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ClientView());
+        }
+
+        private void Transports_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new TransportView());
+        }
+
+        private void Veichles_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new VeichleView());
+        }
+
+        private void Page_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+
+        }
+    }
+}
